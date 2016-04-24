1d0c4246e281a343b06708f81363496addb9b448
4charles2 <ctognol@laposte.net>
2016-04-24 23:04:06 +0200

    mise en relation surface position dans deux struture diffèrente

--- constante.h
+++ constante.h
@@ -6,11 +6,19 @@
  * fourni toutes les structs et complement des fichiers
  * */
 #ifndef
-    typedef struct surface surface
+    typedef struct surface surface;
     struct surface
     {
-        SDL_Surface screen;
+        SDL_Window* screen;
+        pos pos;
         surface* =  next;
     };
-    typedef surface* display
+    typedef surface* display;
+    typedef struct position first;
+    struct position
+    {
+    	SDL_Rect position
+    	position* next;
+    };
+    typedef position *pos;
 #endif
--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "constante.h"
 
 int main(void)
 {
     surface screen;
     ft_sdl_init(screen);
     
+    SDL_Quit();
     return EXIT_SUCCESS;
+}
+
+int ft_sdl_init(surface* screen)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	screen->screen = SDL_CreateWindow("Walbreker", 0, 0, 640, 760, NULL)
 }
